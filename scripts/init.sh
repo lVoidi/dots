742d9db8e2eeb5a03f3dba29eed8f881baf7e453
void <roarba011@gmail.com>
2023-06-08 19:13:11 -0600

    fixed discord_rpc order

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,15 +5,17 @@ function run {
     $@&
   fi
 }
+function discord_rpc {
+  if ! pgrep python3 ;
+  then 
+    python3 ~/.config/awesome/scripts/discord_rich_presence.py
+  fi
+}
 xrandr -r 75
 setxkbmap us -variant altgr-intl
 run /bin/nm-applet
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run picom
-if ! pgrep python3 ;
-then
-  python3 ~/.config/awesome/scripts/discord_rich_presence.py&
-fi
 run mpd
 run pasystray
-# run nitrogen --restore
+discord_rpc
