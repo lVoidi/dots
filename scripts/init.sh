c224e9390df2284a8851f03e6df347536fe82270
sta7ix <lvoidiss@gmail.com>
2024-05-18 15:25:12 -0600

    New wallpaper

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,7 +11,7 @@ function discord_rpc {
     nohup python3 ~/.config/awesome/scripts/discord_rich_presence.py&
   fi
 }
-xrandr -r 75
+xrandr --output DisplayPort-0 --mode 1366x768 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 -r 75 --right-of DisplayPort-0
 setxkbmap us -variant altgr-intl
 run /bin/nm-applet
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
--- ui/widgets/init.lua
+++ ui/widgets/init.lua
@@ -1,6 +1,5 @@
 return {
   tags = require("ui.widgets.workspaces"),
   tasks = require("ui.widgets.tasks"),
-  menu = require("ui.widgets.menu"),
-  volume = require("ui.widgets.volume")
+  menu = require("ui.widgets.menu")
 }
