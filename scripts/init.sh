23fb4c2a1d205f1231d751562cbbe038520e03bc
Arce <r.arce.1@estudiantec.cr>
2025-02-02 13:52:20 -0600

    Added config for gentoo

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,6 +13,7 @@ function discord_rpc {
 }
 xrandr --output DisplayPort-0 --mode 1366x768 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 -r 75 --right-of DisplayPort-0
 setxkbmap us -variant altgr-intl
+run exec gentoo-pipewire-launcher
 run /bin/nm-applet
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run picom
